c8d1706ca224bf07433bc18ce94a8c656c3c0791
Artem Nuzhnyi <tom-corey@yandex.ru>
2023-02-28 22:16:00 +0300

    Test task

--- test_task.py
+++ test_task.py
@@ -1,5 +1,5 @@
 import pickle
-import cv2 as cv
+import cv2 as cv # type: ignore
 import numpy as np
 from tkinter.filedialog import askopenfilename
 import time
@@ -30,14 +30,18 @@ def method_decorator(func: Callable):
 
 class Rules:
 
-    def __init__(self, template: List[str], threshold: float, fig_key: str) -> None:
+    def __init__(self, template: List[str],
+                 threshold: float,
+                 fig_key: str) -> None:
+
         self.template = template
         self.threshold = threshold
         self.fig_key = fig_key
 
     @method_decorator
     def find_matches(self, image) -> List[List[int]]:
-        """Принимает экземпляр класса Rules и производит поиск совпадений по шаблонам
+        """Принимает экземпляр класса Rules
+        и производит поиск совпадений по шаблонам
         возвращает список координат фигуры
         """
         try:
@@ -67,22 +71,29 @@ class Rules:
 
 class Paint:
 
-    def __init__(self, image: np.ndarray, result: List = [], templates: List = []) -> None:
+    def __init__(self, image: np.ndarray,
+                 result: List = [],
+                 templates: List = []) -> None:
+
         self.image = image
         self.templates = templates
         self.result = result
 
     @method_decorator
     def run(self, res_dict: Dict = {}) -> Dict[str, int]:
-        """Принимает список экземпляров класса Rules, вызывает метод find_matches.
-        После чего рисует контуры прямоугольников, сохраняет изображение с контурами и возвращает словарь с координатами фигур
+        """Принимает список экземпляров класса Rules,
+        вызывает метод find_matches.
+        После чего рисует контуры прямоугольников,
+        сохраняет изображение с контурами
+        и возвращает словарь с координатами фигур
         """
         try:
             for template in self.templates:
                 self.result = template.find_matches(self.image)
                 res_dict[template.fig_key] = self.result
                 for (x, y, w, h) in self.result:
-                    cv.rectangle(self.image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                    cv.rectangle(self.image, (x, y), (x + w, y + h),
+                                 (0, 0, 255), 2)
                 cv.imwrite('output1.jpeg', self.image)
             return res_dict
 
@@ -91,7 +102,9 @@ class Paint:
 
     @method_decorator
     def read_pickle(self) -> None:
-        """Создает список экземпляров класса Rules из файлов и передает в метод run"""
+        """Создает список экземпляров класса Rules
+        из файлов и передает в метод run
+        """
         try:
             files = listdir(mypath)
             pickles = filter(lambda x: x.endswith('.pickle'), files)
